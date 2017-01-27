aa1c4d11e2f7508ff6b348bf7af13d2c109a71b0
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-01-27 15:56:44 +1100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return React.createElement('div', {className: 'test'});
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        path: 'njs/',
+        filename: 'bundle.js',
+    }
+};
