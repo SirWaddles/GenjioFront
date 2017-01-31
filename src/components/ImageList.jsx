c586e7d0a0c42487ac868b70533f2588fda4b494
Timothy Clissold <sirwaddlesworth06@gmail.com>
2017-01-31 12:17:46 +1100

    added content types

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,68 @@
 import React from 'react';
 import StoreContainer from './Container';
 import ImageStore from '../stores/imagelist';
-import { Card } from 'semantic-ui-react';
+import { Card, Image, Embed, Button } from 'semantic-ui-react';
+
+function getExtension(name) {
+    var re = /(?:\.([^.]+))?$/;
+    return re.exec(name)[1];
+}
+
+const ImageExtensions = ['png', 'jpeg', 'jpg', 'bmp', 'gif'];
+const VideoExtensions = ['mp4', 'flv', 'avi'];
+const TextExtensions = ['txt', 'html', 'xml', 'json'];
 
 class ImageIcon extends React.Component {
     render() {
-        return <Card raised image={"https://i.genj.io/i/" + this.props.image.name} header={this.props.image.name} description={this.props.image.dateUploaded} />;
+        var extension = getExtension(this.props.image.name);
+        var displayElement = false;
+        var imageSource = "https://i.genj.io/i/" + this.props.image.name;
+        if (ImageExtensions.indexOf(extension) != -1) {
+            displayElement = (
+                <Card.Content>
+                    <a href={imageSource}>
+                        <Image src={imageSource} floated='right' />
+                    </a>
+                </Card.Content>
+            );
+        }
+        if (VideoExtensions.indexOf(extension) != -1) {
+            displayElement = (
+                <Card.Content>
+                    <video controls muted className='videoplayer'>
+                        <source src={imageSource} type="video/mp4" />
+                    </video>
+                </Card.Content>
+            );
+        }
+        if (TextExtensions.indexOf(extension) != -1) {
+            displayElement = (
+                <Card.Content>
+                    <Embed url={imageSource} icon='file text outline' />
+                </Card.Content>
+            );
+        }
+        if (displayElement == false) {
+            displayElement = (
+                <Card.Content>
+
+                </Card.Content>
+            );
+        }
+        return (
+            <Card>
+                {displayElement}
+                <Card.Content>
+                    <Card.Header>
+                        <a href={imageSource}>{this.props.image.name}</a>
+                    </Card.Header>
+                    <p>{this.props.image.dateUploaded}</p>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button basic color='red'>Delete</Button>
+                </Card.Content>
+            </Card>
+        )
     }
 }
 
